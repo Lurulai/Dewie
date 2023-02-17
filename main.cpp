241342a9b2a57da613b0948f437d717b79113c21
Lurulai <vikathuisman@gmail.com>
2023-02-17 11:15:01 +0100

    game class implement to main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 //
-// Created by hello on 14/02/2023.
+// Created by saritak on 14/02/2023.
 //
 
-int main(void) {
+#include "Game.h"
+
+int main() {
+    std::srand(static_cast<unsigned>(time(nullptr)));
+
+    // initialise game engine
+    Game dewie;
+
+    // run the program as long as the window is open
+    while (dewie.IsRunning())
+    {
+        // update the game
+        dewie.Update();
+
+        // render the game
+        dewie.Render();
+    }
+
     return 0;
 }
